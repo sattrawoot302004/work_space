07d617c8f69c01f61c4b257eab23749f188da84c
sattrawoot302004 <sattrawoot.p@kkumail.com>
2024-12-01 13:07:51 +0700

    Showing a List of Articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2024_12_01_043746) do
-  create_table "articles", force: :cascade do |t|
+  create_table "artic Article.find(1)les", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
