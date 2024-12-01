689814af37cbc47022f72ce45d992a21f0b9a5bb
sattrawoot302004 <sattrawoot.p@kkumail.com>
2024-12-01 13:43:52 +0700

    Using a Form Builder

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,5 +6,23 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
-  
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def article_params
+      params.expect(article: [:title, :body])
+    end
 end
